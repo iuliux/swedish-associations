542d47db8ccd47b2002e909780a420cfbc02d5c6
Iulius Curt <iulius.curt@gmail.com>
2025-07-17 12:24:21 +0300

    Make language optional

--- server.py
+++ server.py
@@ -45,7 +45,7 @@ async def global_exception_handler(request: Request, exc: Exception):
 class QuestionRequest(BaseModel):
     question: str
     association_id: int
-    language: str # Optional field for language
+    language: str = None # Optional field for language
     threshold: float = MIN_CHUNK_SCORE  # Optional field for threshold
     debug: bool = False  # Optional field to enable debug mode
 
